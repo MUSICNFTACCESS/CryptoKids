9e20ab7a1165cd6fe8dd2b0dc98772a7e53d3702
MUSICNFTACCESS <crimzncipriano@gmail.com>
2025-06-13 00:38:26 -0400

    🛠 Fix static path handling for whitepaper support

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 // Serve index.html
